55d560f8e03e6feb66f08fbffa565ff13a6d03e3
Mutalbe <mutalbecastelao@gmail.com>
2026-05-22 21:47:20 +0200

    commenting STATICFILES_STORAGE dor debug

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -104,7 +104,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ROOT_URLCONF = 'ecommerce_api.urls'
