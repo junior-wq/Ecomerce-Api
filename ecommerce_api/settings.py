b415d487cdbdcab5303728316a2da5757effab0f
Mutalbe <mutalbecastelao@gmail.com>
2026-05-22 22:01:12 +0200

    uncommenting STATICFILES_STORAGE for debug

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -104,7 +104,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ROOT_URLCONF = 'ecommerce_api.urls'
@@ -187,11 +186,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
-STATIC_URL = '/static/'
+
 MEDIA_URL='/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
