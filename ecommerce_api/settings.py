d82796689e1ce372dab57dcd9d07298402b45524
Mutalbe <mutalbecastelao@gmail.com>
2026-05-23 00:55:31 +0200

    fix the cloudinary bug-updated commenting suspecios code

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -195,11 +195,11 @@ CLOUDINARY_STORAGE = {
 }
 
 MEDIA_URL='/media/'
-# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-if ENVIRONMENT=='production':
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-else:
-    MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# if ENVIRONMENT=='production':
+#     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# else:
+#     MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
 
 
